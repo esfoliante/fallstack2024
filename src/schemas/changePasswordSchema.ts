9029c612b8ae3aa0f887770f8fc022d123dcbcab
Miguel Ferreira <1210701@isep.ipp.pt>
2024-11-26 22:10:45 +0000

    hotfix: error updating email

--- src/schemas/changePasswordSchema.ts
+++ src/schemas/changePasswordSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const changePasswordSchema = z.object({
-  email: z.string().email(),
+  email: z.string().min(6),
   password: z.string().min(6),
   confirmPassword: z.string().min(6),
 });
